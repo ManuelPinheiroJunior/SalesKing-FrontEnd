fe0ce2c8a050f80a81e2345cf045b96ac7775dc8
Manuel Junior <manuel.jr@outlook.com>
2023-12-13 03:18:31 +0000

    Fix: My Mock Ajust

--- src/modules/product/__mocks__/productInsert.mock.ts
+++ src/modules/product/__mocks__/productInsert.mock.ts
@@ -1,7 +1,12 @@
 import { InsertProduct } from '../../../shared/dtos/InsertProduct.dto';
 
 export const mockProductInsert: InsertProduct = {
-  image: 'http://image',
-  name: 'MOCK NAME',
-  price: 543,
+  image: 'url_da_imagem',
+  name: 'Nome do Produto',
+  price: 19.99,
+  weight: 1.5,
+  length: 10,
+  height: 5,
+  width: 8,
+  diameter: 3,
 };
